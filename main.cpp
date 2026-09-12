5dfdc16c7e211bacc4f041e221ab97880f93fed4
Tyrasoul <bs3734ssst@gmail.com>
2026-09-12 12:59:59 +0530

    Added print word by word
    + Changed scope of charDelay to local inside main

--- main.cpp
+++ main.cpp
@@ -11,26 +11,49 @@ using std::string;
 using std::this_thread::sleep_for;
 using std::chrono::duration;
 
-const float charDelay = 0.25;
-
 void print(string msg, float delay);
+void printWord(string msg, float delay);
 
 int main()
 {   
     string msg;
 
+    float charDelay = 0.25;
+    float wordDelay = 0.4;
+
     cout << "Please enter a msg : ";
     getline(cin,msg);
 
-    print(msg, charDelay);
+    //add debug here to change chardelay and worddelay
+
+    printWord(msg, wordDelay);
+
+    return 0;
 }
 
-void print(string msg, float delay = charDelay)
-{   int i;
+void print(string msg, float delay)
+{
     for ( char ch : msg)
     {
         sleep_for(duration<float>(delay));
         cout << ch;
     }
 
+}
+
+void printWord(string msg, float delay)
+{
+    for (char ch : msg)
+    {
+        if (ch != ' ')
+        {
+            cout << ch;
+        }
+        else 
+        {
+            cout << " ";
+            sleep_for(duration<float>(delay));
+        }
+    }
+    sleep_for(duration<float>(delay));
 }
